42895f5dcf593e7ad0733e367d1196bee1033ef1
Chris <c.siegel1991@gmail.com>
2019-09-02 14:21:58 -0500

    added character sprites to the player info cards

--- src/components/PlayerInfo.vue
+++ src/components/PlayerInfo.vue
@@ -1,37 +1,41 @@
 <template>
     <div id="playerInfo">
         <div class="card good" v-if="isGood">
+            <img src="@/assets/goodGuy.jpg">
             <p>You're a regular good guy!</p>
             <p>Keep reading this for a second so it's not obvious.</p>
             <p>More text</p>
-            <button v-show="!ready" v-on:click="confirmReady" >Got it. I'm ready!</button>
+            <button v-show="!ready" v-on:click="confirmReady">Got it. I'm ready!</button>
         </div>
         <div class="card bad" v-if="isBad">
+            <img src="@/assets/badGuy.jpg">
             <p>You're a regular bad guy!</p>
             <p>The bad guys are: </p>
             <p v-bind:key="badGuy" v-for="badGuy in badGuys">{{badGuy}}</p>
-            <button v-show="!ready" v-on:click="confirmReady" >Got it. I'm ready!</button>
+            <button v-show="!ready" v-on:click="confirmReady">Got it. I'm ready!</button>
         </div>
         <div class="card merlin" v-if="isMerlin">
+            <img src="@/assets/merlin.jpg">
             <p>You're Merlin!</p>
             <p>The bad guys are: </p>
             <p v-bind:key="badGuy" v-for="badGuy in badGuys">{{badGuy}}</p>
-            <button v-show="!ready" v-on:click="confirmReady" >Got it. I'm ready!</button>
+            <button v-show="!ready" v-on:click="confirmReady">Got it. I'm ready!</button>
         </div>
         <div class="card assassin" v-if="isAssassin">
+            <img src="@/assets/assassin.jpg">
             <p>You're the assassin!</p>
             <p>The bad guys are: </p>
             <p v-bind:key="badGuy" v-for="badGuy in badGuys">{{badGuy}}</p>
-            <button v-show="!ready" v-on:click="confirmReady" >Got it. I'm ready!</button>
+            <button v-show="!ready" v-on:click="confirmReady">Got it. I'm ready!</button>
         </div>
-        <div class="ready" v-show="ready">Great!  Once everybody is ready the first mission will begin.</div>
+        <div class="ready" v-show="ready">Great! Once everybody is ready the first mission will begin.</div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'PlayerInfo',
-        data: function() {
+        data: function () {
             return {
                 ready: false
             }
@@ -44,10 +48,18 @@
             }
         },
         computed: {
-            isGood: function() {return this.good},
-            isBad: function() {return this.bad},
-            isMerlin: function() {return this.merlin},
-            isAssassin: function() {return this.assassin}
+            isGood: function () {
+                return this.good
+            },
+            isBad: function () {
+                return this.bad
+            },
+            isMerlin: function () {
+                return this.merlin
+            },
+            isAssassin: function () {
+                return this.assassin
+            }
         }
     }
 </script>
@@ -60,10 +72,11 @@
 
     .card {
         margin: auto;
-        width: 35%;
+        width: 50%;
         height: 200px;
         color: beige;
         padding: 35px;
+        display: inline-block;
     }
 
     .good {
@@ -84,10 +97,17 @@
 
     .ready {
         margin: auto;
-        width: 35%;
+        width: 50%;
         height: 40px;
         background-color: seagreen;
         color: sandybrown;
         padding: 35px;
+    }
+
+    img {
+        display: block;
+        height: auto;
+        float: left;
+        max-height: 100%;
     }
 </style>
--- src/views/Avalon.vue
+++ src/views/Avalon.vue
@@ -24,7 +24,6 @@
                 return store.getters.getStepOne
             },
             stepTwo: function() {
-                console.log("hitting step two now")
                 return store.getters.getStepTwo
             },
             questInfoDisplay: function() {
