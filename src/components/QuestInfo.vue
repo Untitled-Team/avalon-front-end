7757b1bd337abea9450c1ad449bcd2739691ab82
Chris <c.siegel1991@gmail.com>
2019-09-06 21:28:15 -0500

    added barebones info to quests.  looks bad.

--- src/components/QuestInfo.vue
+++ src/components/QuestInfo.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="questInfo">
         <span class="quest" :key="quest" v-for="quest in missions">
-            <Quest :party-size="quest.numberOfAdventurers"/>
+            <Quest :party-size="quest.numberOfAdventurers" :pass="quest.pass" :votes="quest.votes" :party="quest.players"/>
         </span>
     </div>
 </template>
