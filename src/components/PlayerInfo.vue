357e15a6a7d2d869855097be847088458f603696
Chris <c.siegel1991@gmail.com>
2019-10-13 11:09:52 -0500

    fix tests

--- src/components/PlayerInfo.vue
+++ src/components/PlayerInfo.vue
@@ -1,32 +1,24 @@
 <template>
     <div id="playerInfo">
-        <div v-if="isGood">
+        <div class="card good" v-if="isGood">
             <div class="card-image">
                 <img class="cardBackground" src="@/assets/merlinCardBig.png">
                 <img class="cardPicture" src="@/assets/goodGuyBig.png">
             </div>
-            <div class="info">
-                <p>You're a regular good guy!</p>
-                <p>Keep reading this for a second so it's not obvious.</p>
-                <p>More text</p>
-            </div>
             <button class="button is-small" v-show="!ready" v-on:click="confirmReady">Ready</button>
         </div>
 
-        <div v-if="isBad">
+        <div class="card bad" v-if="isBad">
             <div class="card-image">
                 <img class="cardBackground" src="@/assets/assassinCardBig.png">
                 <img class="cardPicture" src="@/assets/badGuyBig.png">
-            </div>
-            <div class="info">
-                <p>You're a regular bad guy!</p>
             </div>
             <p>The bad guys are: </p>
             <p :key="index" v-for="(badGuy, index) in badGuys">{{badGuy}}</p>
             <button class="button is-small" v-show="!ready" v-on:click="confirmReady">Ready</button>
         </div>
 
-        <div v-if="isMerlin">
+        <div class="card merlin" v-if="isMerlin">
             <div class="card-image">
                 <img class="cardBackground" src="@/assets/merlinCardBig.png">
                 <img class="cardPicture" src="@/assets/merlinBig.png">
@@ -36,7 +28,7 @@
             <button class="button is-small" v-show="!ready" v-on:click="confirmReady">Ready</button>
         </div>
 
-        <div v-if="isAssassin">
+        <div class="card assassin" v-if="isAssassin">
             <div class="card-image">
                 <img class="cardBackground" src="@/assets/assassinCardBig.png">
                 <img class="cardText" src="@/assets/assassinTextBig.png">
