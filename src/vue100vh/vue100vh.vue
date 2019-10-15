ccf35666d8428b0e35e034bc3edfd3e5714f747c
Taylor Brown <taylor.brown.github@gmail.com>
2019-10-14 22:44:44 -0500

    UI changes for Lobby

--- src/vue100vh/vue100vh.vue
+++ src/vue100vh/vue100vh.vue
@@ -18,6 +18,9 @@ export default {
 
     window.addEventListener('resize', this.updateStyle);
 
+    document.addEventListener('touchmove', function (event) {
+      event.preventDefault();
+    }, false);
     // window.addEventListener('scroll', function()  {
     //   // this.updateStyle();
     //
@@ -31,14 +34,23 @@ export default {
     //   //   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     //   // }
     // });
+    // document.addEventListener('touchmove', function(event) {
+    //   event = event.originalEvent || event;
+    //   if (event.scale !== 1) {
+    //     event.preventDefault();
+    //   }
+    // }, false);
 
     document.addEventListener('gesturestart', function (e) {
       e.preventDefault();
-    });
+    }, {passive: false});
   },
   destroyed () {
     window.removeEventListener('resize', this.updateStyle);
     window.removeEventListener('gesturestart', function (event) {
+      event.preventDefault();
+    }, false);
+    window.removeEventListener('touchmove', function (event) {
       event.preventDefault();
     }, false);
   },
