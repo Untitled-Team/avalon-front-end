bac8b27136a938f2c749489179787a7691ecbe48
Taylor Brown <taylor.brown.github@gmail.com>
2019-10-22 22:38:04 -0500

    Add box shadow for the banners

--- src/components/Quest.vue
+++ src/components/Quest.vue
@@ -187,19 +187,27 @@ export default {
         margin: 0 auto;
     }
 
+    .active > .bannerContainer > .firstRow {
+        width: 100%;
+        height: 4px;
+        padding: 0px 5%;
+        margin: 0 auto;
+        box-shadow: -2px 0px rgba(black, 0.3), 2px 0px rgba(black, 0.3);
+    }
+
     .inactive > .bannerContainer > .firstRow {
         width: 100%;
         height: 4px;
         padding: 0px 5%;
         background: $bannerOutline;
         margin: 0 auto;
-        box-shadow: 0px 4px #5B585F;
+        box-shadow: 0px 4px rgba(black, 0.3), -2px 0px rgba(black, 0.3), 2px 0px rgba(black, 0.3);
     }
 
     .inactive > .bannerContainer > .secondRow {
         width: 90%;
         height: 4px;
-        box-shadow: 0px 4px #5B585F;
+        box-shadow: 0px 4px rgba(black, 0.3);
         padding: 0px 5%;
         background: $bannerOutline;
         margin: 0 auto;
@@ -208,7 +216,7 @@ export default {
     .inactive > .bannerContainer > .thirdRow {
         width: 80%;
         height: 4px;
-        box-shadow: 0px 4px #5B585F;
+        box-shadow: 0px 4px rgba(black, 0.3);
         padding: 0px 10%;
         background: $bannerOutline;
         margin: 0 auto;
@@ -217,7 +225,7 @@ export default {
     .inactive > .bannerContainer > .fourthRow {
         width: 60%;
         height: 4px;
-        box-shadow: 0px 4px #5B585F;
+        box-shadow: 0px 4px rgba(black, 0.3);
         padding: 0px 10%;
         background: $bannerOutline;
         margin: 0 auto;
@@ -226,7 +234,7 @@ export default {
     .inactive > .bannerContainer > .fifthRow {
         width: 40%;
         height: 4px;
-        box-shadow: 0px 4px #5B585F;
+        box-shadow: 0px 4px rgba(black, 0.3);
         padding: 0px 17.5%;
         background: $bannerOutline;
         margin: 0 auto;
@@ -235,7 +243,7 @@ export default {
     .inactive > .bannerContainer > .sixthRow {
         width: 5%;
         height: 4px;
-        box-shadow: 0px 4px #5B585F;
+        box-shadow: 0px 4px rgba(black, 0.3);
         padding: 0px 3%;
         background: $bannerOutline;
         margin: 0 auto;
