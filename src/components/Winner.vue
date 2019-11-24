c2c943e5c3772f015bb90182d8f45929695235a5
Chris <c.siegel1991@gmail.com>
2019-11-24 11:21:32 -0600

    added in new assets to winner screen.

--- src/components/Winner.vue
+++ src/components/Winner.vue
@@ -1,20 +1,24 @@
 <template>
-    <div id="winner">
-        <div id="badGuysWin" v-show="badGuysWin" class="bigText">
-            Bad guys win!
+    <div id="winner" :class="{defeat: badGuysWin, victory: goodGuysWin}">
+        <div class="badGuysWin" v-show="badGuysWin">
+            <div class="defeatScreenWrapper">
+                <img class="defeatScreen" src="@/assets/defeatScreen.png">
+            </div>
         </div>
-        <div id="goodGuysWin" v-show="goodGuysWin" class="bigText">
-            Good guys win!
+        <div class="goodGuysWin" v-show="goodGuysWin">
+            <div class="victoryScreenWrapper">
+                <img class="victoryScreen" src="@/assets/victoryScreen.png">
+            </div>
         </div>
-        <div class="mediumText">
-            <div id="merlin">Merlin: {{ gameOverData.merlin }}</div>
-            <div id="goodGuys">Good Guys:
+        <div>
+            <div class="merlin">Merlin: {{ gameOverData.merlin }}</div>
+            <div class="goodGuys">Good Guys:
                 <div :key="index" v-for="(goodGuy, index) in goodGuysSansMerlin">
                     {{ goodGuy}}
                 </div>
             </div>
-            <div id="assassin">Assassin: {{ gameOverData.assassin }}</div>
-            <div id="badGuys">Bad Guys:
+            <div class="assassin">Assassin: {{ gameOverData.assassin }}</div>
+            <div class="badGuys">Bad Guys:
                 <div :key="index" v-for="(badGuy, index) in badGuysSansAssassin">
                     {{ badGuy}}
                 </div>
@@ -45,5 +49,14 @@
 </script>
 
 
-<style scoped>
+<style lang="scss" scoped>
+    @import "../styles/variables";
+
+    .defeat {
+        background-color: $failed;
+    }
+
+    .victory {
+        background-color: $successful;
+    }
 </style>
