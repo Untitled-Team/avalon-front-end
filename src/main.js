f41d6a24d3060d6ee65197fa55c1a970e28e4e80
Taylor Brown <taylor.brown.github@gmail.com>
2020-09-18 11:28:50 -0500

    Disable router entirely

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import VueNativeSock from 'vue-native-websocket'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from "./store/index.js"
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Avalon from "./views/Avalon"
-
-Vue.use(Router)
-
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Avalon
-    }
-  ]
-})
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Avalon from "./views/Avalon"
+//
+// // Vue.use(Router)
+//
+// export default new Router({
+//   mode: 'history',
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Avalon
+//     }
+//   ]
+// })
